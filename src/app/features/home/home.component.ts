bba100c3b93720b6ce99cf5130139cdaf45748b5
emillymacielaraujo <emillymaciel.araujo@ucsal.edu.br>
2026-06-28 12:24:41 -0300

    feat: implementa componente home

---
+++ src/app/features/home/home.component.ts
@@ -0,0 +1,22 @@
+import { Component, inject } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+import { AuthService } from '../../core/services/auth.service';
+import { NavbarComponent } from '../navbar/navbar.component';
+
+@Component({
+  selector: 'app-home',
+  standalone: true,
+  imports: [CommonModule, RouterModule, NavbarComponent],
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
+})
+export class HomeComponent {
+  private authService = inject(AuthService);
+
+  currentUser$ = this.authService.currentUser$;
+
+  logout(): void {
+    this.authService.logout();
+  }
+}
