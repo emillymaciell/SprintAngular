1cd0d4a4b0fcbc04afd3d707635a6312b515c063
emillymacielaraujo <emillymaciel.araujo@ucsal.edu.br>
2026-06-28 12:21:57 -0300

    feat: adiciona interfaces User, Vehicle e VehicleData

---
+++ src/app/core/models/models.ts
@@ -0,0 +1,23 @@
+export interface User {
+  id: number;
+  nome: string;
+  email: string;
+}
+
+export interface Vehicle {
+  id: number;
+  vehicle: string;
+  volumetotal: number;
+  connected: number;
+  softwareUpdates: number;
+  img: string;
+}
+
+export interface VehicleData {
+  id: number;
+  odometro: number;
+  nivelCombustivel: number;
+  status: string;
+  lat: number;
+  long: number;
+}
